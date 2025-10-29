de6a9dded03af56c59bf75e9b937b852e9ac109a
ArthurLopeseAlexandre <ostecs123@gmail.com>
2025-10-29 15:10:41 -0300

    primeiro commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import {div,h1} from 'react'
+import './App.css'
+
+export default function App() {
+
+return(
+  <div class='titulo'>
+    <h2>SEJA BEM VINDO AO SITE</h2>
+    <h1>TZGAMES</h1>
+    <br />
+    <h4><p>O TZGAMES é um site criado para quem ama videogames! Aqui você encontra novidades, <br />curiosidades, notícias, recomendações e conteúdos variados sobre o mundo gamer. Prepare-se para explorar, aprender e se divertir!</p></h4>
+  </div>
+
+)
+
+}
