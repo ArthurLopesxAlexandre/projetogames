6d2e1c80a642f35d5c667f8acb45b5db9c878892
ArthurLopeseAlexandre <ostecs123@gmail.com>
2025-11-26 15:05:22 -0300

    commit dia 26/11/25

--- src/App.jsx
+++ src/App.jsx
@@ -1,59 +1,57 @@
-
-import {div,h1} from 'react'
-import './App.css'
-
+import { useEffect, useState } from "react";
+import "./App.css";
 
 export default function App() {
+  const imagens = [
+    "/assets/img/wp1811277.jpg",
+    "/assets/img/913243.jpg",
+    "/assets/img/call-of-duty-warzone-4k-kzetz7h7t75073ye.jpg",
+    "/assets/img/gta-online-pdz1sc5c5voimbh0.jpg"
+  ];
+
+  const [index, setIndex] = useState(0);
+
+  useEffect(() => {
+    const intervalo = setInterval(() => {
+      setIndex((prevIndex) => (prevIndex + 1) % imagens.length);
+    }, 3000);
+
+    return () => clearInterval(intervalo);
+  }, []);
+
   return (
     <>
-     <div className='descricao'>
-      <h3>Descubra os lançamentos mais esperados, os jogos que estão dominando o momento e análises exclusivas. Encontre sua próxima aventura no TZGAMES!</h3>
-    </div>
-      <nav className="navbar">
-        <div className="logo-container">
-         
-          <h1 className="titulo">TZGAMES</h1>
-          
-        </div>
-       
+      <div
+        className="carrossel-fundo"
+        style={{ backgroundImage: `url(${imagens[index]})` }}
+      >
+        <div className="overlay"></div>
+      </div>
 
-        <ul className="menu">
+      <nav className="navbar">
+        <div className="logo-area">
+          <img
+            src="/assets/img/Sem título.jpg"
+            alt="Logo TZ"
+            className="logo-img"
+          />
+          <h1 className="logo-text">TZGAMES</h1>
+        </div>
+
+        <ul className="nav-links">
           <li><a href="#">Home</a></li>
           <li><a href="#">Página 2</a></li>
           <li><a href="#">Contato</a></li>
         </ul>
       </nav>
 
-      <div className="cards-container">
-      <div className="card">
-      <img src="./assets/gamepad-svgrepo-com.svg" alt="Jogos" className="card-icon" />
-        <h3>Seu jogo perfeito está aqui</h3>
-        <p>
-        Conheça os lançamentos mais esperados e os títulos que estão bombando no momento. 
-        Dicas, análises e recomendações para todos os estilos de jogador.
-        </p>
+      <div className="descricao">
+        <h3>
+          DESCUBRA OS LANÇAMENTOS MAIS ESPERADOS, OS JOGOS QUE ESTÃO
+          DOMINANDO O MOMENTO E ANÁLISES EXCLUSIVAS. ENCONTRE SUA PRÓXIMA
+          AVENTURA NO TZGAMES!
+        </h3>
       </div>
-
-      <div className="card">
-        <img src="./assets/magnifier-svgrepo-com.svg" alt="Notícias" className="card-icon" />
-        <h3>Principais notícias sobre jogos</h3>
-        <p>
-        Fique por dentro das principais novidades do mundo gamer. 
-        Descubra tudo sobre eventos, campeonatos e anúncios que estão movimentando o universo dos games.
-        </p>
-      </div>
-
-      <div className="card">
-        <img src="./assets/plan-list-svgrepo-com.svg" alt="Atualizaçoes" className="card-icon" />
-        <h3>Atualizações dos seus jogos estão aqui</h3>
-        <p>
-        Saiba tudo sobre os novos patches, expansões e melhorias dos seus jogos favoritos. 
-        Atualize-se e descubra o que mudou nas últimas versões.
-        </p>
-      </div>
-    </div>
-    
-   
     </>
   );
 }
